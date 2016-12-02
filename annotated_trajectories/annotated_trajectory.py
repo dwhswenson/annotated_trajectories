1cf27912b4fde8ff671e6aaffb7b026d7d96b3c4
David W.H. Swenson <dwhs@hyperblazer.net>
2016-12-02 16:51:05 +0100

    Docstrings!

--- annotated_trajectories/annotated_trajectory.py
+++ annotated_trajectories/annotated_trajectory.py
@@ -1,7 +1,7 @@
+from collections import namedtuple
 import matplotlib.pyplot as plt
 import openpathsampling as paths
 from openpathsampling.netcdfplus import StorableNamedObject
-from collections import namedtuple
 
 # this hack of making a class from my namedtuple allows me to give it a
 # docstring
@@ -24,7 +24,7 @@ _validation_fields = ['correct', 'false_positive', 'false_negative']
 class ValidationResults(namedtuple('ValidationResults', _validation_fields)):
     """
     Object returned by validation.
-    
+
     Results from comparing frames identified by a proposed state definition
     to the frames identified by the user. Checks for correct results, false
     positives (proposed definition finds frames the user didn't) and false
@@ -50,7 +50,7 @@ class ValidationResults(namedtuple('ValidationResults', _validation_fields)):
 
 class AnnotatedTrajectory(StorableNamedObject):
     """Trajectory with state annotations.
-    
+
     Parameters
     ----------
     trajectory : ``paths.Trajectory``
@@ -60,6 +60,7 @@ class AnnotatedTrajectory(StorableNamedObject):
         2-tuple (begin, end), inclusive
     """
     def __init__(self, trajectory, annotations=None):
+        super(AnnotatedTrajectory, self).__init__()
         self.trajectory = trajectory
         self.annotations = set([])
         self._frame_map = [None]*len(self.trajectory)
@@ -74,7 +75,7 @@ class AnnotatedTrajectory(StorableNamedObject):
         # the namedtuple
         trajectory = dct['trajectory']
         annotations_list = dct['annotations']
-        annotations = [Annotation(a[0], a[1], a[2]) 
+        annotations = [Annotation(a[0], a[1], a[2])
                        for a in annotations_list]
         return cls(trajectory, annotations)
 
@@ -92,7 +93,7 @@ class AnnotatedTrajectory(StorableNamedObject):
         self.annotations |= set(annotations)
         for annotation in annotations:
             state = annotation.state
-            begin  = annotation.begin
+            begin = annotation.begin
             end = annotation.end
             range_tuple = (begin, end)
             range_list = range(begin, end+1)
@@ -137,6 +138,18 @@ class AnnotatedTrajectory(StorableNamedObject):
         return self._frame_map[idx]
 
     def get_segment_idxs(self, label):
+        """Frame indices sorted by segment (from annotation) for label.
+
+        Parameters
+        ----------
+        label : str
+            the annotation label used
+
+        Returns
+        -------
+        list of list of int
+            the frame numbers for each segment from each annotation
+        """
         try:
             all_ranges = self._annotation_dict[label]
         except KeyError:
@@ -147,6 +160,18 @@ class AnnotatedTrajectory(StorableNamedObject):
 
 
     def get_segments(self, label):
+        """Subtrajectories for each annotation segment for label.
+
+        Parameters
+        ----------
+        label : str
+            the annotation label used
+
+        Returns
+        -------
+        list of ``paths.Trajectory``
+            subtrajectories for each annotation
+        """
         try:
             all_ranges = self._annotation_dict[label]
         except KeyError:
@@ -156,6 +181,13 @@ class AnnotatedTrajectory(StorableNamedObject):
         return all_segments
 
     def get_unassigned(self):
+        """Frame indices that are not annotated with any label.
+
+        Returns
+        -------
+        list of int
+            indices of the trajectory not associated with any label
+        """
         unassigned = [k for k in range(len(self._frame_map))
                       if self._frame_map[k] is None]
         return unassigned
@@ -171,8 +203,26 @@ class AnnotatedTrajectory(StorableNamedObject):
 
         Used internally by the :meth:`.validate_states` method. This section
         factored out to facilitate testing.
+
+        Parameters
+        ----------
+        state : ``paths.Volume``
+            the proposed state volume for this particular state
+        state_annotations : list of 2-tuple
+            list of first and final frame from each annotation for the state
+
+        Returns
+        -------
+        correct_idxs : list of int
+            frame indices where the proposed state and annotations agree
+        false_pos_idxs : list of int
+            frame indices identified as in the proposed state, but not in
+            the annotations
+        false_neg_idxs : list of int
+            frame indices labelled in the annotations, but not in the
+            proposed state
         """
-        expected = set(sum([range(a[0], a[1]+1) 
+        expected = set(sum([range(a[0], a[1]+1)
                             for a in state_annotations], []))
         in_state = set([i for i in range(len(self.trajectory))
                         if state(self.trajectory[i])])
@@ -182,6 +232,24 @@ class AnnotatedTrajectory(StorableNamedObject):
         return (correct_idxs, false_pos_idxs, false_neg_idxs)
 
     def validate_states(self, names_to_volumes):
+        """Compare proposed state definitions to annotations.
+
+        Parameters
+        ----------
+        names_to_volumes : dict {str: ``paths.Volume``}
+            dictionary linking label names to proposed state volumes
+
+        Returns
+        -------
+        results : dict {str: :class:`.ValidationResults`}
+            dictionary linking label name to the results of validation,
+            including correct frames, false positive frames, and false
+            negative frames
+        conflicts : dict {str: list of int}
+            dictionary linking label name to list of frames where the
+            annotated label is different from the result of the proposed
+            state.
+        """
         label_keys = set(self.state_names)
         volume_keys = set(names_to_volumes.keys())
         if len(label_keys - volume_keys) > 0:
@@ -206,28 +274,42 @@ class AnnotatedTrajectory(StorableNamedObject):
             })
             conflicts[state_name] = [i for i in idxs[1]  # false pos idxs
                                      if self._frame_map[i] is not None]
-        
+
         return (results, conflicts)
 
 
-def plot_annotated(trajectory, cv, names_to_volumes, names_to_colors, dt=1):
-    import matplotlib.pyplot as plt
+def plot_annotated(trajectory, cv, names_to_volumes, names_to_colors, dt=1.0):
+    """Plot annotated trajectory, marking annotations and proposed volumes.
+
+    Parameters
+    ----------
+    trajectory : :class:`.AnnotatedTrajectory`
+        the trajectory to plot
+    cv : ``paths.CollectiveVariable``
+        the collective variable to use as the y-axes
+    names_to_volumes: dict {str: ``paths.Volume``}
+        dictionary connecting labels to proposed state definitions
+    names_to_colors: dict {str: str}
+        dictionary connecting labels to (matplotlib) colors
+    dt : float
+        timestep (just changes x-axis), default is 1.0
+    """
     plt.plot([i*dt for i in range(len(trajectory.trajectory))],
              cv(trajectory.trajectory), '-k')
     for state_name in trajectory.state_names:
-	ann_segment_idxs = trajectory.get_segment_idxs(state_name)
-	ann_segments = trajectory.get_segments(state_name)
-	for idxs, segs in zip(ann_segment_idxs, ann_segments):
-	    if len(idxs) > 1:
+        ann_segment_idxs = trajectory.get_segment_idxs(state_name)
+        ann_segments = trajectory.get_segments(state_name)
+        for idxs, segs in zip(ann_segment_idxs, ann_segments):
+            if len(idxs) > 1:
                 plt.plot([i*dt for i in idxs], cv(segs), linestyle='-',
                          color=names_to_colors[state_name])
-	    else:
+            else:
                 plt.plot(idxs, cv(segs), marker='+', markersize=10,
                          color=names_to_colors[state_name])
-	state = names_to_volumes[state_name]
-	in_state_idxs = [i for i in range(len(trajectory.trajectory))
+        state = names_to_volumes[state_name]
+        in_state_idxs = [i for i in range(len(trajectory.trajectory))
 			 if state(trajectory.trajectory[i])]
-        plt.plot([i*dt for i in in_state_idxs], 
+        plt.plot([i*dt for i in in_state_idxs],
                  [cv(trajectory.trajectory[i]) for i in in_state_idxs],
                  color=names_to_colors[state_name], marker='o',
                  linestyle='None')
