f33cbc0e97cf8aea8614371bdf688f5d856b02fa
David W.H. Swenson <dwhs@hyperblazer.net>
2019-02-04 19:02:58 +0100

    updates to add autorelease

--- setup.py
+++ setup.py
@@ -55,8 +55,8 @@ full_version = '%(full_version)s'
 git_revision = '%(git_revision)s'
 release = %(isrelease)s
 
-if not release:
-    version = full_version
+# if not release:
+    # version = full_version
 """
     # Adding the git rev number needs to be done inside write_version_py(),
     # otherwise the import of numpy.version messes up the build under Python 3.
